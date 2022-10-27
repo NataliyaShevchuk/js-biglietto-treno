1741e3efa1d3111341975163adfd07a1f4f645a6
NataliyaShevchuk <nata.shevchuk@icloud.com>
2022-10-27 16:05:35 +0200

    aggiunto prezzo per l'età di mezzo

--- js/math.js
+++ js/math.js
@@ -5,8 +5,9 @@ const price = Math.round(userKm * .21)
 console.log("Km che il passeggero desidera percorrere:", userKm);
 console.log("L'età del passeggero:", userAge);
 
-
-if (userAge < 18 ){
+if ( userAge >= 18 && < 65 ){
+    console.log("Il prezzo è di:", price);
+} else if (userAge < 18 ){
     console.log("Il passeggero è minorenne, pagherà:", ( price * .8));
 } else if (userAge >= 65 ){
     console.log("Il passeggero è over 65, pagherà:", ( price * .6));
